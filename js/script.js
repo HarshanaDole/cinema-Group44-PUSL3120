6ad7f7e7fa647c92de50ef25cd789f90140fb815
Harshana <harudole@gmail.com>
2024-01-02 22:25:06 +0530

    home page done

--- js/script.js
+++ js/script.js
@@ -27,3 +27,35 @@ function contentLoadedCallback() {
 
 includeHTML("header-container", "components/header.html", contentLoadedCallback);
 includeHTML("footer-container", "components/footer.html", contentLoadedCallback);
+
+const filterIcon = document.getElementById("filter-icon");
+const filterPopup = document.getElementById("filterPopup");
+const closeIcon = document.querySelector(".close");
+
+filterIcon.addEventListener("click", () => {
+  togglePopup();
+});
+
+closeIcon.addEventListener("click", () => {
+  togglePopup();
+});
+
+document.body.addEventListener("click", (event) => {
+  if (
+    !filterPopup.contains(event.target) &&
+    event.target !== filterIcon
+  ) {
+    filterPopup.style.display = "none";
+  }
+});
+
+function togglePopup() {
+  if (
+    filterPopup.style.display === "flex" ||
+    filterPopup.style.display === ""
+  ) {
+    filterPopup.style.display = "none";
+  } else {
+    filterPopup.style.display = "flex";
+  }
+}
